a4e257a2d7c60b7fba49e7e4c6af20601b8a1bfd
Bradley English <bradley.english3@gmail.com>
2025-12-08 16:14:58 -0500

    day1part2 done

--- Day 1/main.rb
+++ Day 1/main.rb
@@ -1,5 +1,4 @@
-
-def solvePart1(lines, testMode)
+def solvePart1(lines, verbose)
   answer = 0
   dialPosition = 50
   nDialPositions = 100
@@ -16,11 +15,42 @@ def solvePart1(lines, testMode)
   return answer
 end
 
-def solvePart2(lines, testMode)
+def solvePart2(lines, verbose)
   answer = 0
+  dialPosition = 50
+  nDialPositions = 100
   lines.each do |line|
-    # puts line
-    answer += 1
+    isNegative = line.start_with?("L")
+    quantity = line[1..-1].to_i
+    twistAmt = isNegative ? quantity * -1 : quantity
+    oldDialPosition = dialPosition
+    oldAnswer = answer
+    dialPosition += twistAmt
+    if dialPosition < 0
+      nFullTurns = (dialPosition / nDialPositions.to_f).ceil
+      answer += nFullTurns.abs
+      if oldDialPosition > 0
+        answer += 1
+      end
+    elsif dialPosition > 0
+      nFullTurns = dialPosition / nDialPositions
+      answer += nFullTurns
+    else
+      answer += 1
+    end
+    dialPosition %= nDialPositions
+    if verbose
+      puts "#{oldDialPosition} -(#{twistAmt})-> #{dialPosition} = #{answer - oldAnswer}"
+    end
   end
   return answer
 end
+
+
+# Types of 0 crossings:
+# - land on 0
+# - cross while turning left
+#   - result will always be negative
+#   - answer will be increased by at least 1 and more based on integer division
+# - cross while turning right
+#   - integer division alone will tell you the increase
