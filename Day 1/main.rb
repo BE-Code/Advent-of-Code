a582609088cb516c65f494e904f6b49c61c9de30
Bradley English <bradley.english3@gmail.com>
2025-12-08 15:27:13 -0500

    day1part1 done

--- Day 1/main.rb
+++ Day 1/main.rb
@@ -1,9 +1,17 @@
 
 def solvePart1(lines, testMode)
   answer = 0
+  dialPosition = 50
+  nDialPositions = 100
   lines.each do |line|
-    # puts line
-    answer += 1
+    isNegative = line.start_with?("L")
+    quantity = line[1..-1].to_i
+    twistAmt = isNegative ? quantity * -1 : quantity
+    dialPosition += twistAmt
+    dialPosition %= nDialPositions
+    if dialPosition == 0
+      answer += 1
+    end
   end
   return answer
 end
