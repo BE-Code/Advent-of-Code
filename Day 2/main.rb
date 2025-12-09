11077e43b9e5399048854969abc9b3776cb1c82d
Bradley English <bradley.english3@gmail.com>
2025-12-09 12:49:40 -0500

    solved day2part1

--- Day 2/main.rb
+++ Day 2/main.rb
@@ -1,9 +1,35 @@
+def nDigits(n)
+  return n.to_s.length
+end
+
 def solvePart1(ranges, verbose)
-  puts ranges.to_json
   answer = 0
+  # skipExcept = 3
   ranges.each do |range|
-    # puts range.to_json
-    answer += 1
+    # skipExcept -= 1
+    # if skipExcept != 0
+    #   next
+    # end
+    i = range[0].to_i
+    while i <= range[1].to_i
+      (verbose) ? puts("i: #{i}") : nil
+      if nDigits(i).odd?
+        i = 10**nDigits(i)
+        next
+      end
+      frontHalf = i.to_s[0..(nDigits(i) / 2 - 1)]
+      backHalf = i.to_s[(nDigits(i) / 2)..-1]
+      if frontHalf == backHalf
+        answer += i
+      end
+      if backHalf < frontHalf
+        i = (frontHalf + frontHalf).to_i
+      else
+        newFrontHalf = (frontHalf.to_i + 1).to_s
+        i = (newFrontHalf + newFrontHalf).to_i
+      end
+    end
+    (verbose) ? puts("final i: #{i}") : nil
   end
   return answer
 end
