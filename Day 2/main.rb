c129a0086111fd688efdbd4960ddf8fc1e47572e
Bradley English <bradley.english3@gmail.com>
2025-12-09 10:56:33 -0500

    day2 input

---
+++ Day 2/main.rb
@@ -0,0 +1,18 @@
+def solvePart1(ranges, verbose)
+  puts ranges.to_json
+  answer = 0
+  ranges.each do |range|
+    # puts range.to_json
+    answer += 1
+  end
+  return answer
+end
+
+def solvePart2(ranges, verbose)
+  answer = 0
+  ranges.each do |range|
+    # puts range.to_json
+    answer += 1
+  end
+  return answer
+end
