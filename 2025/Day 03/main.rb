032cc2af19084be62f239e80568cfe2b2095ad7b
Bradley English <bradley.english3@gmail.com>
2025-12-10 01:18:26 -0500

    solve day3part1

--- 2025/Day 03/main.rb
+++ 2025/Day 03/main.rb
@@ -1,8 +1,20 @@
+def findBiggestNumberIndex(nString)
+  biggestIndex = 0
+  for i in 1..nString.length - 1
+    if nString[i].to_i > nString[biggestIndex].to_i
+      biggestIndex = i
+    end
+  end
+  return biggestIndex
+end
+
 def solvePart1(lines, verbose)
   answer = 0
   lines.each do |line|
-    # puts line
-    answer += 1
+    biggestIndex = findBiggestNumberIndex(line[0..-2])
+    secondBiggestIndex = findBiggestNumberIndex(line[biggestIndex + 1..-1]) + biggestIndex.to_i + 1
+    (verbose) ? puts("biggestIndex: #{biggestIndex}, secondBiggestIndex: #{secondBiggestIndex}") : nil
+    answer += (line[biggestIndex] + line[secondBiggestIndex]).to_i
   end
   return answer
 end
