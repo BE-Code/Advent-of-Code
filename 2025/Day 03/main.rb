e6e619d717aeefda40d899397a2d06cc4574f097
Bradley English <bradley.english3@gmail.com>
2025-12-10 01:06:40 -0500

    set up day3

---
+++ 2025/Day 03/main.rb
@@ -0,0 +1,17 @@
+def solvePart1(lines, verbose)
+  answer = 0
+  lines.each do |line|
+    # puts line
+    answer += 1
+  end
+  return answer
+end
+
+def solvePart2(lines, verbose)
+  answer = 0
+  lines.each do |line|
+    # puts line
+    answer += 1
+  end
+  return answer
+end
