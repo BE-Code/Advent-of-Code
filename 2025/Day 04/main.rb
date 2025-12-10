b0126ae4d3956ebe6d7ee148fb33d4581d58ad3f
Bradley English <bradley.english3@gmail.com>
2025-12-10 01:46:23 -0500

    solve day4part2

--- 2025/Day 04/main.rb
+++ 2025/Day 04/main.rb
@@ -1,3 +1,34 @@
+def getAdjacentRolls(lines, i, j)
+  adjacentRolls = 0
+  if j > 0 && lines[i][j] == lines[i][j - 1]
+    adjacentRolls += 1
+  end
+  if j < lines[i].length - 1 && lines[i][j] == lines[i][j + 1]
+    adjacentRolls += 1
+  end
+  if i > 0 && lines[i][j] == lines[i - 1][j]
+    adjacentRolls += 1
+  end
+  if i < lines.length - 1 && lines[i][j] == lines[i + 1][j]
+    adjacentRolls += 1
+  end
+
+  # diagonals
+  if i > 0 && j > 0 && lines[i][j] == lines[i - 1][j - 1]
+    adjacentRolls += 1
+  end
+  if i > 0 && j < lines[i].length - 1 && lines[i][j] == lines[i - 1][j + 1]
+    adjacentRolls += 1
+  end
+  if i < lines.length - 1 && j > 0 && lines[i][j] == lines[i + 1][j - 1]
+    adjacentRolls += 1
+  end
+  if i < lines.length - 1 && j < lines[i].length - 1 && lines[i][j] == lines[i + 1][j + 1]
+    adjacentRolls += 1
+  end
+  return adjacentRolls
+end
+
 def solvePart1(lines, verbose)
   answer = 0
   for i in 0..lines[0].length - 1
@@ -5,33 +36,7 @@ def solvePart1(lines, verbose)
       if lines[i][j] == "."
         next
       end
-      adjacentRolls = 0
-      if j > 0 && lines[i][j] == lines[i][j - 1]
-        adjacentRolls += 1
-      end
-      if j < lines[i].length - 1 && lines[i][j] == lines[i][j + 1]
-        adjacentRolls += 1
-      end
-      if i > 0 && lines[i][j] == lines[i - 1][j]
-        adjacentRolls += 1
-      end
-      if i < lines.length - 1 && lines[i][j] == lines[i + 1][j]
-        adjacentRolls += 1
-      end
-
-      # diagonals
-      if i > 0 && j > 0 && lines[i][j] == lines[i - 1][j - 1]
-        adjacentRolls += 1
-      end
-      if i > 0 && j < lines[i].length - 1 && lines[i][j] == lines[i - 1][j + 1]
-        adjacentRolls += 1
-      end
-      if i < lines.length - 1 && j > 0 && lines[i][j] == lines[i + 1][j - 1]
-        adjacentRolls += 1
-      end
-      if i < lines.length - 1 && j < lines[i].length - 1 && lines[i][j] == lines[i + 1][j + 1]
-        adjacentRolls += 1
-      end
+      adjacentRolls = getAdjacentRolls(lines, i, j)
       if adjacentRolls < 4
         answer += 1
       end
@@ -42,9 +47,25 @@ end
 
 def solvePart2(lines, verbose)
   answer = 0
-  lines.each do |line|
-    # puts line
-    answer += 1
+  while true
+    startingAnswer = answer
+    newLines = lines.map(&:dup)
+    for i in 0..lines[0].length - 1
+      for j in 0..lines[i].length - 1
+        if lines[i][j] == "."
+          next
+        end
+        adjacentRolls = getAdjacentRolls(lines, i, j)
+        if adjacentRolls < 4
+          answer += 1
+          newLines[i][j] = "."
+        end
+      end
+    end
+    lines = newLines
+    if startingAnswer == answer
+      break
+    end
   end
   return answer
 end
