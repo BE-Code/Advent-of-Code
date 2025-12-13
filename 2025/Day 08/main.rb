2dcb7ae9cf304c798cecd8345c9adc0494f822b6
Bradley English <bradley.english3@gmail.com>
2025-12-12 23:18:20 -0500

    gen d8

---
+++ 2025/Day 08/main.rb
@@ -0,0 +1,17 @@
+def solvePart1(boxes, verbose)
+  answer = 0
+  boxes.each do |box|
+    # puts line
+    answer += 1
+  end
+  return answer
+end
+
+def solvePart2(boxes, verbose)
+  answer = 0
+  boxes.each do |box|
+    # puts line
+    answer += 1
+  end
+  return answer
+end
