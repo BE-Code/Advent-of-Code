b96f2841fab1cc86f0abc9dbe227703679a35cde
Bradley English <bradley.english3@gmail.com>
2025-12-14 17:09:01 -0500

    d11p1 solve

--- 2025/Day 11/main.rb
+++ 2025/Day 11/main.rb
@@ -1,11 +1,22 @@
-def solvePart1(lines)
-  puts(lines.to_json())
-
-  answer = 0
-  lines.each do |line|
-    answer += 1
+def numPathsToOutput(start, links)
+  if links[start].include?("out") then return 1 end
+  pathsFound = 0
+  for dest in links[start]
+    pathsFound += numPathsToOutput(dest, links)
   end
-  return answer
+  return pathsFound
+end
+
+def solvePart1(lines)
+  # puts(lines.to_json())
+  links = {}
+  for line in lines
+    links[line[0][0]] = line[1]
+  end
+  # puts(links.to_json())
+
+  numPaths = numPathsToOutput("you", links)
+  return numPaths
 end
 
 def solvePart2(lines)
