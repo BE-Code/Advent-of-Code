78644503462700e2fdd6f31ccb283e21a18397e5
Bradley English <bradley.english3@gmail.com>
2025-12-15 10:11:23 -0500

    d11p2 attempt

--- 2025/Day 11/main.rb
+++ 2025/Day 11/main.rb
@@ -19,12 +19,42 @@ def solvePart1(lines)
   return numPaths
 end
 
-def solvePart2(lines)
-  puts(lines.to_json())
-
-  answer = 0
-  lines.each do |line|
-    answer += 1
+def numPathsToOutput2(start, links, endNode = "out", visitedDAC = false, visitedFFT = false, visitedNodes = [])
+  visitedNodes.push(start)
+  if start == "dac" then visitedDAC = true end
+  if start == "fft" then visitedFFT = true end
+  if links[start].include?("out")
+    return (visitedDAC && visitedFFT) ? 1 : 0
   end
-  return answer
+  pathsFound = 0
+  for dest in links[start]
+    if visitedNodes.include?(dest)
+      puts("found loop", visitedNodes.to_json(), "next dest: #{dest}")
+      next
+    end
+    pathsFound += numPathsToOutput2(dest, links, endNode, visitedDAC, visitedFFT, visitedNodes.clone())
+  end
+  return pathsFound
+end
+
+# Unfinished
+def solvePart2(lines)
+  # puts(lines.to_json())
+  links = {}
+  for line in lines
+    links[line[0][0]] = line[1]
+  end
+  # puts(links.to_json())
+
+  fftToDac = numPathsToOutput2("fft", links, "dac", true, true)
+  puts("fftToDac: #{fftToDac}")
+  fftToDac *= numPathsToOutput2("dac", links, "out", true, true)
+  puts("dacToOut: #{fftToDac}")
+
+  # dacToFft = numPathsToOutput2("dac", links, "fft", true, true)
+  # dacToFft *= numPathsToOutput2("fft", links, "out", true, true)
+  # puts("dacToFft: #{dacToFft}")
+
+  # numPaths = numPathsToOutput2("svr", links)
+  return numPaths
 end
