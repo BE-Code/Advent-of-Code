b9dcfa8821e137be3dc99f4530f9a0abafb08bad
Bradley English <bradley.english3@gmail.com>
2025-12-22 12:23:25 -0700

    add 12

---
+++ 2025/Day 12/main.rb
@@ -0,0 +1,19 @@
+def solvePart1(lines)
+  puts(lines.to_json())
+
+  answer = 0
+  lines.each do |line|
+    answer += 1
+  end
+  return answer
+end
+
+def solvePart2(lines)
+  puts(lines.to_json())
+
+  answer = 0
+  lines.each do |line|
+    answer += 1
+  end
+  return answer
+end
